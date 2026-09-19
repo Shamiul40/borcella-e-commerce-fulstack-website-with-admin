297131fa05dc2ab325b305389c1fd11b5ccf3441
Shamiul40 <shamiul4040@gmail.com>
2026-09-19 22:53:11 +0600

    sign in added

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,13 @@
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>shamiul islam</h1>
+
+      <UserButton />
+
+      
+    </div>
+  );
+}
